1e9239ab0d90acc3222fd691b61a3a5a768db387
Dambthirteen <danijasonlipke@gmail.com>
2025-11-14 20:05:31 +0100

    PlayerController Ready

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,31 +2,73 @@ using UnityEngine;
 using UnityEngine.Rendering;
 
 [RequireComponent(typeof(CharacterController))]
-public class PlayerController : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    //Classes
-    CharacterController characterController;
+    public CharacterController controller;
+    public Transform cam;
 
-    //
+    public float speed = 0.2f;
+    public float WalkSpeed = 0.2f;
+    public float SprintSpeed;
+    public float gravity = -9.81f;
+
+    Vector3 velocity;
     Vector3 move;
 
-    //Settings
-    public float Speed;
-    public float SprintSpeed = 1;
-    public float JumpHeight;
+    bool Sprint;
 
     void Start()
     {
-        characterController = GetComponent<CharacterController>();
+        
     }
 
     void Update()
     {
-        move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        Sprint = Input.GetKey("left shift");
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 forward = cam.forward;
+        Vector3 right = cam.right;
+
+        forward.y = 0;
+        right.y = 0;
+
+        forward.Normalize();
+        right.Normalize();
+
+        move = (right * h + forward * v).normalized;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (controller.isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+
+        sprint();
     }
 
     void FixedUpdate()
     {
-        characterController.Move(move * Speed * SprintSpeed);
+        controller.Move(move * speed);
+    }
+    
+    void sprint()
+    {
+        if (Sprint)
+        {
+            speed = SprintSpeed;
+        }
+        else
+        {
+            speed = WalkSpeed;
+        }
     }
 }
+
