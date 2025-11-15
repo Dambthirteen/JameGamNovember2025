ff796e21ec5bb6b427323449f6438e00bac06e96
Dambthirteen <danijasonlipke@gmail.com>
2025-11-15 13:13:03 +0100

    UI Element Added

--- Assets/Scripts/ControlModule.cs
+++ Assets/Scripts/ControlModule.cs
@@ -22,6 +22,6 @@ public class ControlModule : MonoBehaviour, IInteractable
 
     public string GetDescription()
     {
-        throw new System.NotImplementedException();
+        return "Start Testing";
     }
 }
